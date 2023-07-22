9f64c491a1ee4b3c16cbdc7650a04d42285b75cb
Choiyounho <maxosa78@gmail.com>
2023-07-22 18:43:56 +0900

    docs: domain 모듈 그래이들 적용

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,9 +1,19 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id(Plugin.KOTLIN)
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    implementation(Kotlin.KOTLIN_STDLIB)
+    testImplementation(Junit.JUNIT)
+    testImplementation(Junit.Jupiter.JUPITER)
+    testImplementation(Google.TRUTH)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
